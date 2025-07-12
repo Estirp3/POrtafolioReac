e1f9b298da68cf0849d9eccb641f30969ab3e729
Patricio <patricio.calderonig@gmail.com>
2025-07-12 00:14:36 -0400

    se mejora rendimiento en css y se pone cd publico para descarga

--- src/components/DescaCvCard.jsx
+++ src/components/DescaCvCard.jsx
@@ -1,9 +1,9 @@
 // src/components/DescaCvCard.jsx
 
-import React from 'react';
-import BentoCard from './BentoCard';
-import styles from './DescaCvCard.module.css';
-import DownloadIcon from '../assets/img/descarga.svg'; // Asegúrate de que el path sea correcto
+import React from "react";
+import BentoCard from "./BentoCard";
+import styles from "./DescaCvCard.module.css";
+import DownloadIcon from "../assets/img/descarga.svg"; // Asegúrate de que el path sea correcto
 
 // Eliminamos la función handleDownload, ya no la necesitamos
 
@@ -12,27 +12,30 @@ const DescaCvCard = () => {
     // CAMBIO CLAVE: Usamos una etiqueta <a> como contenedor.
     // href apunta al archivo en la carpeta public.
     // download indica al navegador que descargue el archivo en lugar de navegar a él.
-    <a 
-      href="/Curriculum_Patricio.pdf" // Asegúrate que el nombre de tu archivo CV sea correcto
-      download="Curriculum_PatricioCalderon.pdf" // Nombre con el que se descargará el archivo
+    <a
+      href="/CV_Patricio_Calderon.pdf" // Corregimos el error tipográfico en .pdf
+      download="CV_Patricio_Calderon.pdf"
       className={`${styles.cvCard} ${styles.flipContainer}`}
     >
-      
       <div className={styles.flipper}>
-        
         {/* Cara Frontal */}
         <div className={styles.front}>
           <h3 className={styles.title}>Descargar CV</h3>
-          <img src={DownloadIcon} alt="Download CV Icon" className={styles.icon} />
-          <p className={styles.description}>Haz clic para obtener mi Currículum.</p>
+          <img
+            src={DownloadIcon}
+            alt="Download CV Icon"
+            className={styles.icon}
+          />
+          <p className={styles.description}>
+            Haz clic para obtener mi Currículum.
+          </p>
         </div>
-        
+
         {/* Cara Trasera */}
         <div className={styles.back}>
           <p className={styles.legend}>Descargar CV de Patricio Calderón</p>
         </div>
       </div>
-      
     </a>
   );
 };
